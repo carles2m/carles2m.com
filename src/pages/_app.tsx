ac4b579f9246e87ff947ae9a676eda5895f787e0
Carles Moreno <carlesm@outlook.com>
2021-10-13 14:39:05 -0700

    Add template service worker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
-import React from 'react'
-import Head  from 'next/head'
+import React, { useEffect } from 'react'
+import Head from 'next/head'
+import * as serviceWorker from "../../lib/serviceWorker"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://cra.link/PWA
+    serviceWorker.unregister();
+  }, []);
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
+import React from 'react'
 
 export default class Document extends NextDocument {
   render() {
@@ -7,6 +8,8 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="apple-touch-icon" href="logo192.png" />
+          <link rel="manifest" href="manifest.json" />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
