9b3f8e1142065ff4fe96797babecdd9d80733f3d
Carles Moreno <carlesm@outlook.com>
2021-10-15 15:28:53 -0700

    Add missing meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <meta name="description" content="Carles Moreno's personal site, where his projects, skills and experience are summarized" />
           <link rel="shortcut icon" href="favicon.ico" />
           <link rel="apple-touch-icon" href="logo192.png" />
           <link rel="manifest" href="manifest.json" />
