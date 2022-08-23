bde87c7020cd3fc00a5b41100cd1f153c5dc448e
Carles Moreno <carlesm@outlook.com>
2022-08-23 08:52:25 -0700

    Update next.config.js with new required pattern

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching
+});
+
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching
-  }
+  // other next.js config
 });
