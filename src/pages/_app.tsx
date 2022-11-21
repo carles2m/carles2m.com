91fb9f17d038e8673f5ef4fed66561e1bbce6090
Carles Moreno <carlesm@outlook.com>
2022-11-20 16:55:01 -0800

    Prevent duplicate toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,17 +14,21 @@ import { Chakra } from "../lib/Chakra";
 import { GA_TRACKING_ID, GA_TRACKING_ID_2 } from "../lib/gtag";
 
 const { ToastContainer, toast } = createStandaloneToast();
+const pwaToastId = "pwa-toast";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   if (typeof (navigator) !== "undefined" && "serviceWorker" in navigator) {
     navigator.serviceWorker.ready.then(() => {
-      toast({
-        title: <Text as="span"><ChakraLink variant="bw" textDecor="underline" isExternal href="https://developer.mozilla.org/docs/Web/Progressive_web_apps">PWA</ChakraLink> active</Text>,
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-        position: "top"
-      });
+      if (!toast.isActive(pwaToastId)) {
+        toast({
+          id: pwaToastId,
+          title: <Text as="span"><ChakraLink variant="bw" textDecor="underline" isExternal href="https://developer.mozilla.org/docs/Web/Progressive_web_apps">PWA</ChakraLink> active</Text>,
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "top"
+        });
+      }
     });
   }
 
