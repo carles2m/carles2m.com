975fe8118e2764d89501c7a93fe4969b55bcf51f
Carles Moreno <carlesm@outlook.com>
2024-03-03 11:24:06 -0800

    Remove google analytics reporting

--- src/lib/gtag.ts
+++
@@ -1,9 +0,0 @@
-export const GA_TRACKING_ID = "G-2L1ZXMPH7Y";
-
-export const reportClick = (url: string) => {
-  window.gtag("event", "click", {
-    "event_category": "outbound",
-    "event_label": url,
-    "transport_type": "beacon"
-  });
-};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { Partytown } from "@builder.io/partytown/react";
 import {
   createStandaloneToast,
   Link as ChakraLink,
@@ -6,16 +5,14 @@ import {
 } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import type { AppProps, NextWebVitalsMetric } from "next/app";
+import type { AppProps } from "next/app";
 import Head from "next/head";
-import Script from "next/script";
 import { NextSeo } from "next-seo";
 import React from "react";
 
 import SEO from "../../next-seo.config";
 import { Chakra } from "../components/Chakra";
 import { profileName } from "../lib/content";
-import { GA_TRACKING_ID } from "../lib/gtag";
 
 const { ToastContainer, toast } = createStandaloneToast();
 const pwaToastId = "pwa-toast";
@@ -47,44 +44,15 @@ const MyApp: React.FC<AppProps> = ({
         <Head>
           <title>{profileName}</title>
           <meta name="viewport" content="initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
-
-          <Partytown debug={false} forward={["dataLayer.push", "gtag"]} />
-          <script
-            id="google-analytics"
-            type="text/partytown"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                window.gtag = function(){window.dataLayer.push(arguments);};
-                window.gtag('js', new Date());
-                window.gtag('config', '${GA_TRACKING_ID}');
-              `
-            }}
-          />
         </Head>
 
         <Component {...pageProps} />
 
         <SpeedInsights />
         <Analytics />
-        <Script
-          type="text/partytown"
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
       </Chakra>
     </>
   );
 };
-
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  console.log(metric);
-
-  window.gtag("event", metric.name, {
-    event_category: metric.label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(metric.name === "CLS" ? metric.value * 1000 : metric.value), // values must be integers
-    event_label: metric.id, // id unique to current page load
-    non_interaction: true, // avoids affecting bounce rate.
-  });
-}
 
 export default MyApp;
